2ac5df1db8bb3d6523f9a417c95e85a0c762b850
davidsantosai <davidutsuka@gmail.com>
2022-11-11 17:29:17 -0500

    Implemented DELETE and started with PUT

--- src/handlers/alerts.js
+++ src/handlers/alerts.js
@@ -192,12 +192,156 @@ module.exports = {
      * Required data: id
      * Optional data: none
      */
+    const id =
+      data.queryStringObject.id &&
+      typeof data.queryStringObject.id === "string" &&
+      data.queryStringObject.id.trim().length === 21
+        ? data.queryStringObject.id.trim()
+        : false;
+
+    if (!id) {
+      callback(400, { status: 400, error: "Missing required field" });
+    } else {
+      _data.read("alerts", id, (error, alertData) => {
+        if (error || !alertData) {
+          callback(404, { status: 404, error: "Alert not found" });
+        } else {
+          const token =
+            typeof data.headers.token === "string" ? data.headers.token : false;
+
+          if (!token) {
+            callback(403, { status: 403, error: "Forbidden" });
+          } else {
+            verifyToken(token, alertData.userPhone, (valid) => {
+              if (!valid) {
+                callback(403, { status: 403, error: "Forbidden" });
+              } else {
+                /*TO DO: Delete the Alert in alerts folder + Delete the alert in the user and update it*/
+                _data.delete("alerts", id, (error) => {
+                  if (error) {
+                    callback(500, { status: 500, error: "Server Error" });
+                  } else {
+                    _data.read(
+                      "users",
+                      alertData.userPhone,
+                      (error, userData) => {
+                        if (error || !userData) {
+                          callback(500, { status: 500, error: "Server Error" });
+                        } else {
+                          const userAlerts =
+                            userData.alerts &&
+                            typeof userData.alerts === "object" &&
+                            userData.alerts instanceof Array
+                              ? userData.alerts
+                              : [];
+
+                          const alertPosition = userAlerts.indexOf(id);
+
+                          if (alertPosition < 0) {
+                            callback(500, {
+                              status: 500,
+                              error: "Server Error",
+                            });
+                          } else {
+                            userAlerts.splice(alertPosition, 1);
+                            userData.alerts = userAlerts;
+                            _data.update(
+                              "users",
+                              alertData.userPhone,
+                              userData,
+                              (error) => {
+                                if (error) {
+                                  callback(500, {
+                                    status: 500,
+                                    error: "Server Error",
+                                  });
+                                } else {
+                                  callback(200, { status: 200, data: "OK" });
+                                }
+                              }
+                            );
+                          }
+                        }
+                      }
+                    );
+                  }
+                });
+              }
+            });
+          }
+        }
+      });
+    }
   },
   put: function (data, callback) {
     /**
      * Alert - put
      * Required data: id
-     * Optional data: none
+     * Optional data: protocol,url,method,successCodes or timeoutSeconds
      */
+
+    const id =
+      data.payload.id &&
+      typeof data.payload.id === "string" &&
+      data.payload.id.trim().length === 21
+        ? data.payload.id
+        : false;
+
+    const updateAlert = {};
+
+    data.payload.protocol &&
+    typeof data.payload.protocol === "string" &&
+    ["http", "https"].includes(data.payload.protocol)
+      ? (updateAlert.protocol = data.payload.protocol)
+      : false;
+
+    data.payload.url &&
+    typeof data.payload.url === "string" &&
+    data.payload.url.trim().length > 0
+      ? (updateAlert.url = data.payload.url.trim())
+      : false;
+
+    data.payload.method &&
+    typeof data.payload.method === "string" &&
+    ["get", "post", "put", "delete"].includes(data.payload.method)
+      ? (updateAlert.method = data.payload.method)
+      : false;
+
+    data.payload.successCodes &&
+    typeof data.payload.successCodes === "object" &&
+    data.payload.successCodes instanceof Array &&
+    data.payload.successCodes.length
+      ? (updateAlert.successCodes = data.payload.successCodes)
+      : false;
+
+    data.payload.timeoutSeconds &&
+    typeof data.payload.timeoutSeconds === "number" &&
+    data.payload.timeoutSeconds % 1 &&
+    data.payload.timeoutSeconds > 3 &&
+    data.payload.timeoutSeconds < 8
+      ? (updateAlert.timeoutSeconds = data.payload.timeoutSeconds)
+      : false;
+
+    if (!id) {
+      callback(400, { status: 400, error: "Missing valid id" });
+    } else {
+      _data.read("alerts", id, (error, alertData) => {
+        if (error || !alertData) {
+          callback(500, { status: 500, error: "Server Error" });
+        } else {
+          const token =
+            typeof data.headers.token === "string" ? data.headers.token : false;
+          if (!token) {
+            callback(403, { status: 403, error: "Forbidden" });
+          } else {
+            verifyToken(token, alertData.userPhone, (valid) => {
+              if (!valid) {
+                callback(403, { status: 403, error: "Forbidden" });
+              }
+            });
+          }
+        }
+      });
+    }
   },
 };
